b8141a32fd2f16ad005cff1ddb4a33e6a53fecae
Ui Sam <sam.santos@intmed.com.br>
2022-08-03 23:00:07 -0300

    feat: home page

--- frontend/src/containers/Home.js
+++ frontend/src/containers/Home.js
@@ -1,8 +1,15 @@
-import React from "react";
+import React from 'react';
+import { Link } from 'react-router-dom'
 
 const Home = () => (
-  <div>
-    Home
+  <div className='container'>
+    <div class='jumbotron mt-5'>
+        <h1 class='display-4'>Bem Vindo ao Sistema de Login do Sam! </h1>
+        <p class='lead'>Esse sistema foi desenvolvido para o PDI do Sam, não tem valor comercial. Esse sistema foi feito com o coração ❤️.</p>
+        <hr class='my-4' />
+        <p></p>
+        <Link class='btn btn-primary btn-lg' to='/login' role='button'>Login</Link>
+    </div>
   </div>  
 );
 
